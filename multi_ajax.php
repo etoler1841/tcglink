0b762ce350cb592f3414826b4ebac0f28eafe17e
Eric Toler <etoler1841@gmail.com>
2018-06-07 12:18:08 -0500

    Fixed AJAX errors, created handler for file upload

--- multi_ajax.php
+++ multi_ajax.php
@@ -18,7 +18,14 @@
       }
       break;
     case 'desc':
-      $newName = $data['prodName']." (".$data['val'].")";
+      $baseName = substr($data['prodName'], 0, strpos($data['prodName'], ' ['));
+      $cardName = $baseName." (".$data['val'].")";
+      $altName = str_replace(array(" ", "/", "\\"), "-", str_replace(array(",", ".", "!", "?", "'", "(", ")", ":"), "", str_replace(array(" - ", " // "), "", $cardName)));
+      $imgName = $altName.'.jpg';
+      $newBaseName = $conn->real_escape_string($baseName);
+      $newName = $conn->real_escape_string($cardName);
+      $setCode = substr($data['prodName'], strpos($data['prodName'], "[")+1, strpos($data['prodName'], "]")-strpos($data['prodName'], "[")-1);
+      $model = substr($setCode.'-'.$altName, 0, 32);
       $stmt = "UPDATE products_description
                SET products_name = '$newName'
                WHERE products_id = ".$data['prodId'];
@@ -26,25 +33,44 @@
       if($conn->error){
         $return['status'] = 'err';
         $return['errors'][] = $conn->error;
+        $return['query'][] = $stmt;
       } else {
-        $stmt = "SELECT cd.multiverse_id
-                 FROM mtg_card_data cd
-                 LEFT JOIN mtg_sets s ON cd.set_name = s.set_name
-                 WHERE card_name = '".$data['prodName']."'
-                 AND s.categories_id IN (
-                   SELECT master_categories_id FROM products WHERE products_id = ".$data['prodId']."
-                 )";
-        $row = $conn->query($stmt)->fetch_array(MYSQLI_NUM);
-        $multiverseId = $row[0];
-        $stmt = "UPDATE mtg_card_link
-                 SET multiverse_id = $multiverseId
+        $stmt = "UPDATE products
+                 SET products_model = '$model',
+                     products_image = 'mtgsingles/$setCode/$imgName'
                  WHERE products_id = ".$data['prodId'];
         $conn->query($stmt);
         if($conn->error){
           $return['status'] = 'err';
           $return['errors'][] = $conn->error;
+          $return['query'][] = $stmt;
         } else {
-          $return['status'] = 'ok';
+          $stmt = "SELECT cd.multiverse_id
+                   FROM mtg_card_data cd
+                   LEFT JOIN mtg_sets s ON cd.set_name = s.set_name
+                   WHERE cd.card_name = '".$newBaseName."'
+                   AND s.categories_id IN (
+                     SELECT master_categories_id FROM products WHERE products_id = ".$data['prodId']."
+                   )";
+          $row = $conn->query($stmt)->fetch_array(MYSQLI_NUM);
+          $return['query'][] = $stmt;
+          if($conn->error){
+            $return['status'] = 'err';
+            $return['errors'][] = $conn->error;
+          } else {
+            $multiverseId = $row[0];
+            $stmt = "UPDATE mtg_card_link
+                     SET multiverse_id = $multiverseId
+                     WHERE products_id = ".$data['prodId'];
+            $conn->query($stmt);
+            if($conn->error){
+              $return['status'] = 'err';
+              $return['errors'][] = $conn->error;
+              $return['query'][] = $stmt;
+            } else {
+              $return['status'] = 'ok';
+            }
+          }
         }
       }
       break;
