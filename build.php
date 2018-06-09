0c7998453b5529db13da4f29721d535a6585307e
Eric Toler <etoler1841@gmail.com>
2018-06-08 21:30:46 -0500

    Fixed one more stupid error

--- build.php
+++ build.php
@@ -79,10 +79,10 @@
             $conn->query($stmt);
 
             //mtg_card_link
-            $stmt = "UPDATE mtg_card_link
+            $stmt = "INSERT INTO mtg_card_link
                      SET products_id = $prodId,
-                         is_foil = ".((isset($normal)) ? 0 : 1)."
-                     WHERE tcgp_id = $tcgpId";
+                         tcgp_id = $tcgpId,
+                         is_foil = ".((isset($normal)) ? 0 : 1);
             $conn->query($stmt);
 
             $ch2 = curl_init();
