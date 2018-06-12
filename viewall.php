ac3fe4b448e9b7f6a297ac0fc2786ce4d2823df7
Eric Toler <etoler1841@gmail.com>
2018-06-11 21:35:49 -0500

    Small tweaks to improve function on viewall

--- viewall.php
+++ viewall.php
@@ -22,14 +22,15 @@
   asort($sets);
 
   if(isset($_GET['group'])){
-    $i = 0;
+    $i = $j = 0;
     do {
       $values = "groupId=".$_GET['group']."&offset=".($i*100)."&limit=100";
       curl_setopt($ch, CURLOPT_URL, "http://api.tcgplayer.com/catalog/products?".$values);
       $response = json_decode(curl_exec($ch));
       foreach($response->results as $card){
         $cards[$card->productId] = array(
-          'name' => $card->productName
+          'name' => $card->productName,
+          'img' => $card->image
         );
         $ids[] = $card->productId;
       }
@@ -40,6 +41,7 @@
     if(isset($cards)){
       $i = 0;
       while($i < sizeof($ids)) {
+        $search = array();
         for($j = 0; $j < 100; $i++, $j++){
           if($i === sizeof($ids)){
             break;
@@ -68,9 +70,26 @@
     td, th {
       padding: 5px;
     }
+
+    #cards img {
+      height: 35px;
+    }
+
+    #img-div {
+      position: fixed;
+      top: 0;
+      right: 0;
+      display: none;
+    }
+
+    #img-div img {
+      height: 350px;
+      width: auto;
+    }
   </style>
 </head>
 <body>
+  <div id="img-div"></div>
   <form action="" method="get">
     <select name='group'>
       <option value=''>Choose....</option>
@@ -85,6 +104,7 @@
     <thead>
       <tr>
         <th>ID</th>
+        <th>Image</th>
         <th>Name</th>
         <th>subTypeName</th>
         <th>Market Price</th>
@@ -93,25 +113,37 @@
     </thead>
     <tbody>
     <?php foreach($cards as $id => $data){
-      if(isset($data['prices']['Normal']) && ($data['prices']['Normal']['market'] != 0 || $data['prices']['Normal']['mid'] != 0)){
+    if($data['prices']['Normal']['market'] != 0 || $data['prices']['Normal']['mid'] != 0){
         echo "<tr>
-        <td>".$id."</td>
-        <td>".$data['name']."</td>
-        <td>Normal</td>
-        <td>$".$data['prices']['Normal']['market']."</td>
-        <td>$".$data['prices']['Normal']['mid']."</td>
+          <td>".$id."</td>
+          <td><img src='".$data['img']."' onerror='this.style.display=\"none\"' /></td>
+          <td>".$data['name']."</td>
+          <td>Normal</td>
+          <td>$".$data['prices']['Normal']['market']."</td>
+          <td>$".$data['prices']['Normal']['mid']."</td>
         </tr>";
-      }
-      if(isset($data['prices']['Foil']) && ($data['prices']['Foil']['market'] != 0 || $data['prices']['Foil']['mid'] != 0)){
+    }
+    if($data['prices']['Foil']['market'] != 0 || $data['prices']['Foil']['mid'] != 0){
         echo "<tr>
-        <td>".$id."</td>
-        <td>".$data['name']."</td>
-        <td>Foil</td>
-        <td>$".$data['prices']['Foil']['market']."</td>
-        <td>$".$data['prices']['Foil']['mid']."</td>
+          <td>".$id."</td>
+          <td><img src='".$data['img']."' onerror='this.style.display=\"none\"' /></td>
+          <td>".$data['name']."</td>
+          <td>Foil</td>
+          <td>$".$data['prices']['Foil']['market']."</td>
+          <td>$".$data['prices']['Foil']['mid']."</td>
         </tr>";
       }
     } ?>
     </tbody>
   </table>
 </body>
+<script>
+  $("#cards img").on("mouseover", (e) => {
+    $("#img-div").html(`<img src="${$(e.currentTarget).attr("src")}" />`);
+    $("#img-div").show();
+  });
+
+  $("#cards img").on("mouseout", (e) => {
+    $("#img-div").hide();
+  });
+</script>
