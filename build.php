606ccaedaa16b99818783b01e1a8ee8474f98fa7
Eric Toler <etoler1841@gmail.com>
2018-06-18 20:20:23 -0500

    Built tool to get missing cards from existing sets

--- build.php
+++ build.php
@@ -92,60 +92,67 @@
 
             //Save the product image
             $file = file_get_contents($card->image);
-            if(!is_dir("../../images/mtgsingles/$setCode")){
-              mkdir("../../images/mtgsingles/$setCode");
+            if(!is_dir($imgPath."/mtgsingles/$setCode")){
+              mkdir($imgPath."/mtgsingles/$setCode");
             }
-            file_put_contents("../../images/mtgsingles/$setCode/$imgName", $file);
+            file_put_contents($imgPath."/mtgsingles/$setCode/$imgName", $file);
 
             //Create foil if needed (a.k.a. "Second verse, same as the first!")
             if(isset($foil) && isset($normal)){
               $cardName = $conn->real_escape_string($card->productName.' - Foil');
+              $stmt = "SELECT 1
+                       FROM products p
+                       LEFT JOIN products_description pd ON p.products_id = pd.products_id
+                       WHERE pd.products_name = '$cardName'
+                       AND p.master_categories_id = $catId";
+              $result = $conn->query($stmt);
+              if(!$result->num_rows){
+                //Prepare variables for products tables
+                $hyphenName = str_replace(array(" ", "/", "\\"), "-", str_replace(array(",", ".", "!", "?", "'", "(", ")", ":"), "", str_replace(array(" - ", " // "), "", $card->productName)));
+                $imgName = $hyphenName.'.jpg';
+                $model = str_replace("--", "-", substr($setCode.'-'.$hyphenName, 0, 27).'-Foil');
 
-              //Prepare variables for products tables
-              $hyphenName = str_replace(array(" ", "/", "\\"), "-", str_replace(array(",", ".", "!", "?", "'", "(", ")", ":"), "", str_replace(array(" - ", " // "), "", $card->productName)));
-              $imgName = $hyphenName.'.jpg';
-              $model = str_replace("--", "-", substr($setCode.'-'.$hyphenName, 0, 27).'-Foil');
+                //products
+                $stmt = "INSERT INTO products
+                         SET products_model = '$model',
+                             products_image = 'mtgsingles/$setCode/$imgName',
+                             products_date_added = '".date("Y-m-d H:i:s")."',
+                             products_weight = 0.004,
+                             products_status = 0,
+                             products_qty_box_status = 1,
+                             manufacturers_id = 0,
+                             products_quantity_mixed = 1,
+                             master_categories_id = $catId,
+                             img_update = 1,
+                             products_full_name = '$cardName'";
+                $conn->query($stmt);
+                $prodId = $conn->insert_id;
+                $return['result']['prodId_foil'] = $prodId;
 
-              //products
-              $stmt = "INSERT INTO products
-                       SET products_model = '$model',
-                           products_image = 'mtgsingles/$setCode/$imgName',
-                           products_date_added = '".date("Y-m-d H:i:s")."',
-                           products_weight = 0.004,
-                           products_status = 0,
-                           products_qty_box_status = 1,
-                           manufacturers_id = 0,
-                           products_quantity_mixed = 1,
-                           master_categories_id = $catId,
-                           img_update = 1,
-                           products_full_name = '$cardName'";
-              $conn->query($stmt);
-              $prodId = $conn->insert_id;
-              $return['result']['prodId_foil'] = $prodId;
+                //products_description
+                $stmt = "INSERT INTO products_description
+                         SET products_id = $prodId,
+                             products_name = '$cardName'";
+                $conn->query($stmt);
 
-              //products_description
-              $stmt = "INSERT INTO products_description
-                       SET products_id = $prodId,
-                           products_name = '$cardName'";
-              $conn->query($stmt);
+                //products_to_categories
+                $stmt = "INSERT INTO products_to_categories
+                         SET products_id = $prodId,
+                             categories_id = $catId";
+                $conn->query($stmt);
 
-              //products_to_categories
-              $stmt = "INSERT INTO products_to_categories
-                       SET products_id = $prodId,
-                           categories_id = $catId";
-              $conn->query($stmt);
+                //mtg_card_link
+                $stmt = "INSERT INTO mtg_card_link
+                         SET products_id = $prodId,
+                             tcgp_id = $tcgpId,
+                             is_foil = 1";
+                $conn->query($stmt);
 
-              //mtg_card_link
-              $stmt = "INSERT INTO mtg_card_link
-                       SET products_id = $prodId,
-                           tcgp_id = $tcgpId,
-                           is_foil = 1";
-              $conn->query($stmt);
-
-              $ch2 = curl_init();
-              curl_setopt($ch2, CURLOPT_URL, "$path/update_item.php?prodId=".$prodId);
-              curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
-              $a = curl_exec($ch2);
+                $ch2 = curl_init();
+                curl_setopt($ch2, CURLOPT_URL, "$path/update_item.php?prodId=".$prodId);
+                curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+                $a = curl_exec($ch2);
+              }
             }
             $return['status'] = 'ok';
           } else {
--- missing_cards.php
+++ missing_cards.php
@@ -1,7 +1,8 @@
 <?php
   define("SITE_ROOT", '.');
   require(SITE_ROOT.'/includes/includes.php');
-  $start = (isset($_GET['start'])) ? $_GET['start'] : 0 ;
+  $page = (isset($_GET['page'])) ? $_GET['page'] : 0;
+  $limit = (isset($_GET['limit'])) ? $_GET['limit'] : 20;
 
   $ch = $ch2 = curl_init();
   $headers = array(
@@ -9,39 +10,53 @@
   );
   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  $stmt = "SELECT tcgp_id FROM mtg_sets ORDER BY set_name ASC LIMIT $start, 1";
-  $stmt2 = $conn->prepare("SELECT 1 FROM mtg_card_link WHERE tcgp_id = ?");
+  $stmt = "SELECT tcgp_id
+           FROM mtg_sets";
+  if(isset($_GET['catId'])) $stmt .= " WHERE categories_id = ".$_GET['catId'];
+  $stmt .= " ORDER BY set_name ASC
+             LIMIT ".($page*$limit).", 1";
+  $stmt2 = $conn->prepare("SELECT 1
+                           FROM mtg_card_link cl
+                           LEFT JOIN products_description pd ON cl.products_id = pd.products_id
+                           WHERE cl.tcgp_id = ?
+                           AND pd.products_name NOT LIKE '%- Foil'");
   $result = $conn->query($stmt);
   if($result->num_rows == 0){
     exit("Finished");
   }
   while($row = $result->fetch_array(MYSQLI_NUM)){
-    $stop = false;
-    $i = $j = 0;
-    while(!$stop){
-      $values = 'groupId='.$row[0].'&limit=100&offset='.$i;
+    $i = $j = $k = 0;
+    do {
+      $values = 'groupId='.$row[0].'&limit='.$limit.'&offset='.($i+($page*$limit));
       curl_setopt($ch, CURLOPT_URL, "http://api.tcgplayer.com/catalog/products?$values");
       $data = json_decode(curl_exec($ch));
-      if(sizeof($data->results) == 0){
-        $stop = true;
-      }
       foreach($data->results as $card){
         $i++;
         $stmt2->bind_param("i", $card->productId);
+        $stmt2->store_result();
         $stmt2->execute();
         $stmt2->store_result();
         if($stmt2->num_rows == 0){
-          curl_setopt($ch2, CURLOPT_URL, "http://www.pricebustersgames.com/pbadmin/tcglink/build_cards.php?tcgpId=".$card->productId);
+          curl_setopt($ch2, CURLOPT_URL, $path."/build.php?tcgpId=".$card->productId);
+          $response2 = json_decode(curl_exec($ch2));
+          curl_setopt($ch2, CURLOPT_URL, $path."/gatherer_scrape.php?prodId=".$response2->result->prodId."&tcgpId=".$card->productId);
           curl_exec($ch2);
+          if(isset($response2->result->prodId_foil)){
+            curl_setopt($ch2, CURLOPT_URL, $path."/gatherer_scrape.php?prodId=".$response2->result->prodId_foil."&tcgpId=".$card->productId);
+            curl_exec($ch2);
+            $k++;
+          }
           $j++;
         }
       }
-    }
+    } while ($i < $limit);
   }
-  echo "$i cards found. $j cards built.";
+  echo "$i cards found. $j cards built, plus $k foils.";
 ?>
 <script>
   $(window).load(() => {
-    window.location.href = "?start=<?=$start+1?>";
+    let url = "?page=<?=$page+1?>&limit=<?=$limit?>";
+    if(<?php echo isset($_GET['catId']) ? 'true' : 'false' ;?>) url += "&catId=<?php if(isset($_GET['catId'])) echo $_GET['catId'];?>";
+    window.location.href = (url);
   });
 </script>
