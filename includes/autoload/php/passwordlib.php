98cb6b9d367b78b9b70272f6554139a169726006
Eric Toler <etoler1841@gmail.com>
2018-06-20 15:05:55 -0500

    Tweaking Password library

--- includes/autoload/php/passwordlib.php
+++ includes/autoload/php/passwordlib.php
@@ -3,9 +3,9 @@
  * PHP 5.5-like password hashing functions
  *
  * Provides a password_hash() and password_verify() function as appeared in PHP 5.5.0
- * 
+ *
  * See: http://php.net/password_hash and http://php.net/password_verify
- * 
+ *
  * @link https://github.com/Antnee/phpPasswordHashingLib
  */
 
@@ -15,20 +15,20 @@ if (!function_exists('password_hash')){
     function password_hash($password, $algo=PASSWORD_DEFAULT, $options=array()){
         $crypt = new PhpPasswordLib;
         $crypt->setAlgorithm($algo);
-        
+
         $debug  = isset($options['debug'])
                 ? $options['debug']
                 : NULL;
-        
+
         $password = $crypt->generateCryptPassword($password, $options, $debug);
-        
+
         return $password;
     }
 }
 
 if (!function_exists('password_verify')){
-    function password_verify($password, $hash){
-        return (crypt($password, $hash) === $hash);
+    function password_verify($password, $verify, $hash){
+        return (crypt($password, $hash) === $verify);
     }
 }
 
